231724671626a44b69311d0f1459524ea057c388
Brian Holdsworth <brian@2c996026-adaf-49ee-8809-d55d424b804e>
2024-02-11 11:29:29 -0500

    Sign package

--- buildpkg.sh
+++ buildpkg.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
+export GPGKEY=48FF70B8434078A7F830E720D91EF4A55F9D3B3C
 export NODE_OPTIONS=--openssl-legacy-provider
 make build-frontend
 make build-backend
-makepkg -cf --config makepkg.conf
+makepkg -cf --config makepkg.conf --sign
